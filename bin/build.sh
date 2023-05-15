20d4bbbc2a92f5c806806c7c4749efb6187d54cb
Ian Meyers <meyersi@amazon.com>
2023-05-15 15:23:29 +0100

    Fixing alias issue

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "build" ]; then
-  rm -Rf dist build && python -m build
+  rm -Rf dist build && python3 -m build
   twine check dist/*
 elif [ "$1" == "deploy-test" ]; then
   twine upload --repository-url https://test.pypi.org/legacy/ dist/*
